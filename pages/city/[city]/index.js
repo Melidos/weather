ff8f64633770cc800f690ac02945034ff0fa2675
Kevin BESSON <kevin.besson.719@gmail.com>
2021-10-05 19:22:03 +0200

    Added variable background color depending on weather

--- pages/city/[city]/index.js
+++ pages/city/[city]/index.js
@@ -6,12 +6,30 @@ import Head from "next/head";
 import styles from "../../../styles/city.index.module.css";
 
 export default function City(props) {
-  useEffect(() => {
-    console.log(props);
-  });
+  const BACKGROUNDS = {
+    Thunderstorm: "linear-gradient(#3f2c7b, #936cc7)",
+    Drizzle: "linear-gradient(#1E4B47, #8DCAC4)",
+    Rain: "linear-gradient(#1E4B47, #8DCAC4)",
+    Snow: "linear-gradient(#F8F8F8, #B8B8B8)",
+    Clear: "linear-gradient(#49A6DF, #EEF8FE)",
+    Clouds: "linear-gradient(#7B8D93, #BECCCD)",
+    Mist: "linear-gradient(#7B8D93, #BECCCD)",
+    Smoke: "linear-gradient(#7B8D93, #BECCCD)",
+    Haze: "linear-gradient(#7B8D93, #BECCCD)",
+    Dust: "linear-gradient(#CFA068, #D9AB72)",
+    Fog: "linear-gradient(#7B8D93, #BECCCD)",
+    Sand: "linear-gradient(#CFA068, #D9AB72)",
+    Ash: "linear-gradient(#756456, #A99187)",
+    Squall: "linear-gradient(#69758B, #64788B)",
+    Tornado: "linear-gradient(#69758B, #64788B)",
+  };
 
   return (
-    <Container fluid id={styles.mainContainer}>
+    <Container
+      fluid
+      id={styles.mainContainer}
+      style={{ background: BACKGROUNDS[props.weather] }}
+    >
       <Head>
         <title>Meteo à {props.name}</title>
       </Head>
@@ -23,18 +41,20 @@ export default function City(props) {
           Mis à jour à {props.hours}:{props.minutes}:{props.seconds}
         </span>
       </div>
-      <Image
-        src={props.icon}
-        className='mx-auto d-block'
-        alt={props.main}
-        id={styles.logoWeather}
-      />
+      <div>
+        <Image
+          src={props.icon}
+          className='mx-auto d-block'
+          alt={props.main}
+          id={styles.logoWeather}
+        />
+        <div id={styles.mainWeather}>{props.main}</div>
+      </div>
       <div id={styles.mainTemp}>{props.temperature}°C</div>
       <div id={styles.secondaryContainer}>
         <div id={styles.minTemp}>{props.temp_min}°C</div>
         <div id={styles.maxTemp}>{props.temp_max}°C</div>
       </div>
-      <div id={styles.mainWeather}>{props.main}</div>
     </Container>
   );
 }
@@ -79,9 +99,15 @@ export async function getStaticProps(params) {
         "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@4x.png",
       weather: data.weather[0].main,
       revalidate: 3600,
-      hours: d.getHours(),
-      minutes: d.getMinutes(),
-      seconds: d.getSeconds(),
+      hours: new Intl.NumberFormat("fr-FR", {
+        minimumIntegerDigits: 2,
+      }).format(d.getHours()),
+      minutes: new Intl.NumberFormat("fr-FR", {
+        minimumIntegerDigits: 2,
+      }).format(d.getMinutes()),
+      seconds: new Intl.NumberFormat("fr-FR", {
+        minimumIntegerDigits: 2,
+      }).format(d.getSeconds()),
     },
   };
 }
