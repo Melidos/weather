1a19e3b7f66dec673c8a68e31e57c7d7c8e5d1a2
Kevin BESSON <kevin.besson.719@gmail.com>
2021-10-25 18:33:49 +0200

    Removed unused import

--- pages/city/[city]/index.js
+++ pages/city/[city]/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
 import { Container, Image } from "react-bootstrap";
 import Head from "next/head";
 
